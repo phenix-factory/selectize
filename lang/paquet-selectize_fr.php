f2b21f7cd1e263bb844944c6eae6fce2fca75bad
Debondt Didier <p@henix.be>
2014-12-17 17:34:03 +0100

    Démarrer le plugin selectize

---
+++ lang/paquet-selectize_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'selectize_description' => '',
+	'selectize_nom' => 'Selectize.js',
+	'selectize_slogan' => 'Intégre la librairie Selectize.js à SPIP',
+);
+
+?>
---
+++ selectize_pipelines.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Utilisations de pipelines par Selectize.js
+ *
+ * @plugin     Selectize.js
+ * @copyright  2014
+ * @author     Vertige (Didier)
+ * @licence    GNU/GPL
+ * @package    SPIP\Selectize\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
+
+
+
+
+?>
