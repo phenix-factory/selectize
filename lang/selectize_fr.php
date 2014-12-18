55e3d440f437cb5c404bbb0fbaf42491d557f7c4
Debondt Didier <p@henix.be>
2014-12-18 10:23:19 +0100

    Untabify

--- lang/selectize_fr.php
+++ lang/selectize_fr.php
@@ -2,19 +2,18 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// S
-	'selectize_titre' => 'Selectize.js',
+    // S
+    'selectize_titre' => 'Selectize.js',
 
-	// C
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
-	'cfg_titre_parametrages' => 'Paramétrages',
+    // C
+    'cfg_exemple' => 'Exemple',
+    'cfg_exemple_explication' => 'Explication de cet exemple',
+    'cfg_titre_parametrages' => 'Paramétrages',
 
-	// T
-	'titre_page_configurer_selectize' => 'Configuration de Selectize',
+    // T
+    'titre_page_configurer_selectize' => 'Configuration de Selectize',
+    'class' => 'Selecteur jQuery pour selectize'
 );
-
-?>    'class' => 'Selecteur jQuery pour selectize'
