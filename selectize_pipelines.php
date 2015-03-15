0b814f51495acf2eb3b2dc1a08e06d320f85f021
Debondt Didier <p@henix.be>
2015-03-15 17:58:34 +0100

    Commentaire et ménage

--- selectize_pipelines.php
+++ selectize_pipelines.php
@@ -11,29 +11,27 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
+// Charger selectize en tant que plugin jQuery
 function selectize_jquery_plugins($scripts) {
     $scripts[] = 'lib/selectize.js/dist/js/selectize.js';
     return $scripts;
 }
 
+// Charger selectize SPIP
 function selectize_insert_head($flux) {
-
     $flux .= '<script type="text/javascript" src="'.produire_fond_statique('javascript/spip.selectize.js').'"></script>';
-
     return $flux;
 }
 
+// Charger le css de Selectize sur l'espace publique
 function selectize_insert_head_css($flux) {
-
     $flux .= '<link rel="stylesheet" href="'.find_in_path('lib/selectize.js/dist/css/selectize.css').'" />';
-
     return $flux;
 }
 
+// Charger selectize dans l'espace privé
 function selectize_header_prive($flux) {
-
     $flux .= '<link rel="stylesheet" href="'.find_in_path('lib/selectize.js/dist/css/selectize.css').'" />';
     $flux .= '<script type="text/javascript" src="'.produire_fond_statique('javascript/spip.selectize.js').'"></script>';
-
     return $flux;
 }
