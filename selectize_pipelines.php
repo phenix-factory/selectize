cabadb1613db4bb60e2a76c0a8f59f18273410c2
Debondt Didier <p@henix.be>
2015-05-31 01:15:12 +0200

    Fichier minimal pour selectize

--- selectize_pipelines.php
+++ selectize_pipelines.php
@@ -13,7 +13,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 // Charger selectize en tant que plugin jQuery
 function selectize_jquery_plugins($scripts) {
-    $scripts[] = 'lib/selectize.js/dist/js/standalone/selectize.js';
+    $scripts[] = 'lib/selectize/selectize.js';
     return $scripts;
 }
 
@@ -25,13 +25,13 @@ function selectize_insert_head($flux) {
 
 // Charger le css de Selectize sur l'espace publique
 function selectize_insert_head_css($flux) {
-    $flux .= '<link rel="stylesheet" href="'.find_in_path('lib/selectize.js/dist/css/selectize.css').'" />';
+    $flux .= '<link rel="stylesheet" href="'.find_in_path('lib/selectize/selectize.css').'" />';
     return $flux;
 }
 
 // Charger selectize dans l'espace privé
 function selectize_header_prive($flux) {
-    $flux .= '<link rel="stylesheet" href="'.find_in_path('lib/selectize.js/dist/css/selectize.css').'" />';
+    $flux .= '<link rel="stylesheet" href="'.find_in_path('lib/selectize/selectize.css').'" />';
     $flux .= '<script type="text/javascript" src="'.produire_fond_statique('javascript/spip.selectize.js').'"></script>';
     return $flux;
 }
